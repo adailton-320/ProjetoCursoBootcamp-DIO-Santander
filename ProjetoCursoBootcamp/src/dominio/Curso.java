6d6c5826fd9f3deac583bbfbb3c38f7d88928a0b
adailton-320 <adailtonjr320@gmail.com>
2024-05-18 22:17:00 -0300

    Adiciona classe conteudo e extende para curso e mentoria.

--- ProjetoCursoBootcamp/src/dominio/Curso.java
+++ ProjetoCursoBootcamp/src/dominio/Curso.java
@@ -1,27 +1,23 @@
 package dominio;
 
-public class Curso {
+import testMain.Conteudo;
+
+public class Curso extends Conteudo {
 	
-	private String titulo;
-	private String descricao;
+	
 	private int cargaHoraria;
 	
 	public Curso() {
 		
 	}
 	
-	public String getTitulo() {
-		return titulo;
+	@Override
+	public double calcularXp() {
+		// TODO Auto-generated method stub
+		return XP_PADRAO * cargaHoraria;
 	}
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
+	
+	
 	public int getCargaHoraria() {
 		return cargaHoraria;
 	}
@@ -30,8 +26,11 @@ public class Curso {
 	}
 	@Override
 	public String toString() {
-		return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+		return "Curso [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", cargaHoraria=" + cargaHoraria + "]";
 	}
+
+
+	
 	
 	
 
--- ProjetoCursoBootcamp/src/dominio/Mentoria.java
+++ ProjetoCursoBootcamp/src/dominio/Mentoria.java
@@ -2,28 +2,24 @@ package dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
+import testMain.Conteudo;
+
+public class Mentoria extends Conteudo {
 	
-	private String titulo;
-	private String descricao;
+	
 	private LocalDate data;
 	
 	public Mentoria() {
 		
 	}
 	
-	public String getTitulo() {
-		return titulo;
+	@Override
+	public double calcularXp() {
+		
+		return XP_PADRAO + 20d;
 	}
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
+	
+	
 	public LocalDate getData() {
 		return data;
 	}
@@ -32,7 +28,7 @@ public class Mentoria {
 	}
 	@Override
 	public String toString() {
-		return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", data=" + data + "]";
+		return "Mentoria [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", data=" + data + "]";
 	}
 	
 	
